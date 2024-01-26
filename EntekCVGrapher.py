da0090942efb6411bf1f6848963453d3f1441bc8
Isaac Triplett <isaactriplett21@live.com>
2024-01-26 00:47:19 -0800

    added axis flipping functionality

--- EntekCVGrapher.py
+++ EntekCVGrapher.py
@@ -14,12 +14,17 @@ from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)     # Configur
 #same general layout as Nyquist grapher. Make sure to replace my file path with your own.
 
 def CV_grapher(const #Normalizing constant to turn current input into current density output if desired
-               ,filenames,title,xlimits,yunits,ylimits,txt): #txt is true if dealing with a txt file, and false if dealing with a csv
-    
+               ,filenames,title,xlimits,yunits,ylimits,txt,currentDependence): #txt is true if dealing with a txt file, and false if dealing with a csv
+#currentDependence: True if current is on the y axis.    
+
     fig = plt.figure(figsize=(8, 6))    # Create a graph 'fig' which has 4 inches in width and 6 inches in height.
     ax = fig.add_subplot(111)           # Create a subplot 'ax' in the figure 'fig'. 
-    ax.set_ylabel('Potential (V)')   # set the label of the x-axis
-    ax.set_xlabel('Current ' + yunits) # set the label of the y-axis
+    if currentDependence == True:
+        ax.set_ylabel('Current ' + yunits)
+        ax.set_xlabel('Potential (V)')
+    else:
+        ax.set_ylabel('Potential (V)')   # set the label of the x-axis
+        ax.set_xlabel('Current ' + yunits) # set the label of the y-axis
     
     X1 = []
     X2 = []
@@ -45,8 +50,12 @@ def CV_grapher(const #Normalizing constant to turn current input into current de
                    usecols=(1,2))
                      # Read data from a file scan1.csv and skip the first row.
         elif txt == True:
-            datalistX[i],datalistY[i]=np.loadtxt(fname="C:/Users/isaac/OneDrive/Documents/Electrochemistry Program/Echem Project/Data/txt files/Week 3/" + filenames[i] + ".txt", skiprows=1, unpack=True,
-                   usecols=(0,1))
+            if currentDependence == True:
+                datalistX[i],datalistY[i]=np.loadtxt(fname="C:/Users/isaac/OneDrive/Documents/Electrochemistry Program/Echem Project/Data/txt files/Week 3/" + filenames[i] + ".txt", skiprows=1, unpack=True,
+                                                     usecols=(0,1))
+            else:
+                datalistY[i],datalistX[i]=np.loadtxt(fname="C:/Users/isaac/OneDrive/Documents/Electrochemistry Program/Echem Project/Data/txt files/Week 3/" + filenames[i] + ".txt", skiprows=1, unpack=True,
+                                                     usecols=(0,1))
         ax.plot(datalistX[i], datalistY[i]/const, '.', color=colorlist[i],label=filenames[i])
         i = i + 1
         
